985994fbfc57e3d3e86ed1b25206023631980a2f
AshokKumarJena <ashokkumar.jena01@gmail.com>
2024-01-09 12:09:01 +0530

    msg

---
+++ src/test/java/ashok/Myfirstclass.java
@@ -0,0 +1,9 @@
+package ashok;
+
+public class Myfirstclass {
+	public static void main(String[] args) {
+		System.out.println("Good morning all my friends");
+	}
+}
+//AshokKumar token: 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
+//uri      :https://github.com/AshokKumarJena/AshokKumar.git
